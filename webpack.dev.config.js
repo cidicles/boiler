e6ed43fefc6c96fa86ee63c4dff166fccfb7dd98
cidicles <admin@ericjseaton.com>
2022-11-10 19:57:27 -0800

    No need for mini css

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,8 +1,6 @@
 const path = require('path');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-  plugins: [new MiniCssExtractPlugin()],
   mode: 'development',
   entry: './src/index.js',
   devServer: {
