5734b023c2c69f21999605f281439ec78d7e7c78
cidicles <admin@ericjseaton.com>
2022-11-11 09:43:38 -0800

    Moved inspiration to a hook

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -1,7 +1,27 @@
 const ESLintPlugin = require('eslint-webpack-plugin');
+const inspiration = ['you can do it', 'amazing', 'wow good job', 'you rock', 'wat a 1337 haxor'];
+const fonts = ['Alligator', 'Crazy', 'Graffiti', 'Line Blocks', 'Relief', 'Train'];
+const randy = yar => yar[Math.floor(Math.random()*yar.length)];
+const figlet = require('figlet');
 
 module.exports = {
-  plugins: [new ESLintPlugin()],
+  plugins: [
+    new ESLintPlugin(),
+    {
+      apply: (compiler) => {
+        compiler.hooks.afterEmit.tap('afterCompile', (compilation) => {
+          figlet.text(randy(inspiration), {
+            font: randy(fonts),
+            horizontalLayout: 'default',
+            verticalLayout: 'default',
+            whitespaceBreak: true
+          }, function(err, data) {
+            console.log(data);
+          });
+        });
+      }
+    }
+  ],
   module: {
     rules: [
       {
--- webpack/webpack.js
+++ webpack/webpack.js
@@ -1,20 +1,6 @@
 const path = require('path');
 const { merge } = require('webpack-merge');
-const figlet = require('figlet');
 const common = require('./webpack.common.js');
-
-const inspiration = ['you can do it', 'amazing', 'wow good job', 'you rock', 'wat a 1337 haxor'];
-const fonts = ['Alligator', 'Crazy', 'Graffiti', 'Line Blocks', 'Relief', 'Train'];
-const randy = yar => yar[Math.floor(Math.random()*yar.length)];
-
-figlet.text(randy(inspiration), {
-  font: randy(fonts),
-  horizontalLayout: 'default',
-  verticalLayout: 'default',
-  whitespaceBreak: true
-}, function(err, data) {
-  console.log(data);
-});
 
 module.exports = merge(common, {
   mode: 'production',
